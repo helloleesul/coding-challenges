1db5396718e2ea9a5dd21b4ffff3992ccc8df1b4
LEE SUL <suerish.e@gmail.com>
2024-01-28 19:39:21 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,5 +1,5 @@
 function solution(num_list) {
-    var answer = num_list.reduce((prev, i)=>{return prev * i});
-    var answer2 = num_list.reduce((prev, i)=>{return prev + i});
+    var answer = num_list.reduce((prev, i)=> prev * i);
+    var answer2 = num_list.reduce((prev, i)=> prev + i);
     return answer < answer2 ** 2 ? 1 : 0;
 }
