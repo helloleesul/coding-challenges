b03ce2322617bb0c281c6cfc017ceed72ae1a10c
LEE SUL <suerish.e@gmail.com>
2024-02-04 18:23:06 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,3 +1,4 @@
 function solution(my_string, letter) {
-    return  [...my_string].filter(i => i !== letter).join('');
+    // return  [...my_string].filter(i => i !== letter).join('');
+    return my_string.replaceAll(letter, '');
 }
