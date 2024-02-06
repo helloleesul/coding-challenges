3195f58c9fc498b23b10fb679234a79e58ba69ab
LEE SUL <suerish.e@gmail.com>
2024-02-06 11:43:54 +0900

    [level 0] Title: 2차원으로 만들기, Time: 3.32 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,8 +1,14 @@
 function solution(num_list, n) {
     const answer = [];
     
-    for(let i = 0; i < num_list.length / n; i++) {
-         answer.push(num_list.slice(i * n, i * n + n));
+    // for(let i = 0; i < num_list.length / n; i++) {
+    //      answer.push(num_list.slice(i * n, i * n + n));
+    // }
+    
+    while(num_list.length) {
+        answer.push(num_list.splice(0,n));
+        console.log(answer, num_list)
     }
+    
     return answer
 }
