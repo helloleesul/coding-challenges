46c3bb9de3789054d4f67e9b2f60416d1022bb04
LEE SUL <suerish.e@gmail.com>
2024-02-08 11:01:09 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,4 +1,6 @@
 function solution(numbers) {
-    numbers.sort((a,b) => b-a);
-    return numbers[0]*numbers[1]
+    // numbers.sort((a,b) => b-a);
+    // return numbers[0]*numbers[1]
+    let [a, b] = numbers.sort((a,b) => b - a);
+    return a * b;
 }
