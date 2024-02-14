2eefa02d590d333de5a6be16df2e5b8fb1320e8d
LEE SUL <suerish.e@gmail.com>
2024-02-14 15:27:35 +0900

    [level 0] Title: 369게임, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -0,0 +1,8 @@
+function solution(order) {
+    const arr = Array.from(String(order),(v)=>Number(v));
+    return arr.reduce((acc,v) => {
+        const game = [3, 6, 9];
+        game.forEach(i => i === v ? (acc += 1) : acc);
+        return acc;
+    }, 0);
+}
