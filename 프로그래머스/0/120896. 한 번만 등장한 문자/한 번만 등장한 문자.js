63b9ab916f8e5884b660e072b248180c6f0f64b8
LEE SUL <suerish.e@gmail.com>
2024-02-16 18:58:29 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,5 +1,7 @@
 function solution(s) {
-    let res = [];
-    for (let c of s) if (s.indexOf(c) === s.lastIndexOf(c)) res.push(c);
-    return res.sort().join('');
+    // let res = [];
+    // for (let c of s) if (s.indexOf(c) === s.lastIndexOf(c)) res.push(c);
+    // return res.sort().join('');
+    const result = [...s].filter(c => s.match(new RegExp(c,'g')).length === 1)
+    return result.sort().join('');
 }
