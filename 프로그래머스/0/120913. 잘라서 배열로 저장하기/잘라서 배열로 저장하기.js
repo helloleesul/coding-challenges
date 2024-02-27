8cb22f736be4f875fcdbf89444032017967255f6
LEE SUL <suerish.e@gmail.com>
2024-02-27 10:41:16 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,10 +1,11 @@
 function solution(my_str, n) {
-    const answer = [];
-    const new_str = [...my_str];
+//     const answer = [];
+//     const new_str = [...my_str];
     
-    while (new_str.length) {
-        answer.push(new_str.splice(0, n).join(''));
-        console.log(new_str, answer);
-    }
-    return answer;
+//     while (new_str.length) {
+//         answer.push(new_str.splice(0, n).join(''));
+//         console.log(new_str, answer);
+//     }
+//     return answer;
+    return my_str.match(new RegExp(`.{1,${n}}`, "g"));
 }
