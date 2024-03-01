65da399607c6f4ac20076000049a629fcb43b749
LEE SUL <suerish.e@gmail.com>
2024-03-01 11:23:51 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,9 +1,12 @@
 function solution(my_string) {
-    const regex = /\d+/g;
-    const matchedNumbers = my_string.match(regex);
+//     const regex = /\d+/g;
+//     const matchedNumbers = my_string.match(regex);
 
-    if (matchedNumbers) {
-        const numbers = matchedNumbers.map(match => Number(match));
-        return numbers.reduce((acc,v)=>acc+=v,0)
-    } else return 0
+//     if (matchedNumbers) {
+//         const numbers = matchedNumbers.map(match => Number(match));
+//         return numbers.reduce((acc,v)=>acc+=v,0)
+//     } else return 0;
+    
+    return my_string.split(/\D+/).reduce((acc, cur) => acc + Number(cur), 0);
+    // 연속된 문자들만 없애고 배열로 남은 숫자 더하기
 }
